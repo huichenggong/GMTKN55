65fd83b1a00e9426d31a1b1b5ceac3fef08ad0bf
Chenggong cobra <chenggong.hui@mpibpc.mpg.de>
2021-10-11 21:53:31 +0200

    prepare orca_charge_mult.txt in each folder

---
+++ non-Co/Charge_Multiplicity.sh
@@ -0,0 +1,16 @@
+for name in RG18 ADIM6 S22 S66 HEAVY28 WATER27 CARBHB12 PNICO23 HAL59 AHB21 CHB6 IL16 \
+            IDISP ICONF ACONF Amino20x4 PCONF21 MCONF SCONF UPU23 BUT14DIOL
+do
+    echo "##############"
+    echo "CHARGE_MULTIPLICITY_$name.txt"
+    echo "##############"
+    while read p; do
+        #echo "$p"
+        IFS=' '     # space is set as delimiter
+        read -ra ADDR <<< "$p"   # str is read into an array as tokens separated by IFS
+        #ls $name/${ADDR[0]}
+        echo "* xyzfile" "${ADDR[1]}" "${ADDR[2]}" "struc.xyz" > $name/${ADDR[0]}/orca_charge_mult.txt
+    done <CHARGE_MULTIPLICITY_$name.txt
+    
+    #break
+done
