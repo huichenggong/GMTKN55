ae2be959d4778b39f236434bb12da4c20caa8d71
Chenggong cobra <chenggong.hui@mpibpc.mpg.de>
2021-10-11 21:52:32 +0200

    download struct

---
+++ non-Co/download.sh
@@ -0,0 +1,8 @@
+for name in RG18 ADIM6 S22 S66 HEAVY28 WATER27 CARBHB12 PNICO23 HAL59 AHB21 CHB6 IL16 \
+            IDISP ICONF ACONF Amino20x4 PCONF21 MCONF SCONF UPU23 BUT14DIOL
+do
+    wget www.thch.uni-bonn.de/tc.old/downloads/GMTKN/GMTKN55/$name.tar
+    tar xvf $name.tar
+    wget www.thch.uni-bonn.de/tc.old/downloads/GMTKN/GMTKN55/CHARGE_MULTIPLICITY_$name.txt
+done
+
