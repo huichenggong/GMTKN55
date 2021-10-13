8c3ec23aed8fda43a96e6a04b282600d4c2f3d1f
huichenggong-E29 <1902745538@qq.com>
2021-10-13 13:56:29 +0200

    comment added

--- non-Co/Charge_Multiplicity.sh
+++ non-Co/Charge_Multiplicity.sh
@@ -1,3 +1,14 @@
+#####################################
+#
+#Run download.sh before runing this
+#
+# This script will write the 
+# * xyzfile charge multiplicity struc.xyz
+# line in orca input file name as orca_charge_mult.txt in each structure folders
+#
+# The orca_charge_mult.txt will later be assembled into the full orca input file
+#
+#####################################
 for name in RG18 ADIM6 S22 S66 HEAVY28 WATER27 CARBHB12 PNICO23 HAL59 AHB21 CHB6 IL16 \
             IDISP ICONF ACONF Amino20x4 PCONF21 MCONF SCONF UPU23 BUT14DIOL
 do
--- non-Co/download.sh
+++ non-Co/download.sh
@@ -1,3 +1,8 @@
+#####################################
+#
+#Run this script first to download the structure in GMTKN55 from www.thch.uni-bonn.de
+#
+#####################################
 for name in RG18 ADIM6 S22 S66 HEAVY28 WATER27 CARBHB12 PNICO23 HAL59 AHB21 CHB6 IL16 \
             IDISP ICONF ACONF Amino20x4 PCONF21 MCONF SCONF UPU23 BUT14DIOL
 do
